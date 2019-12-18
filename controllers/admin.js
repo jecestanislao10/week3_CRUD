67077182f487d4269ef0546511774e824f999a5c
jecestanislao10 <jestanislao@stratpoint.com>
2019-12-18 14:55:58 +0800

    updated getUser function

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const {validationResult} = require('express-validator/check');
 
 exports.getUsers = async (req, res, next) => {
-    if(req.permissionLevel!== 'admin')
+    if(req.permissionLevel!== '1')
         {
         const error = new Error ('operation not allowed');
         error.statusCode = 405;
